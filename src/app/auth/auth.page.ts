e64dcd9e44a22e6c4eade277ab597f1fc24e59d6
Tzunac <antolagana003@gmail.com>
2021-04-09 15:55:42 +0200

    aggiunta di classe utente di prova

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataUtenti, Utente } from './auth.service';
+//import { DataUtenti, Utente } from '../user.model';
 
 @Component({
   selector: 'app-auth',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthPage implements OnInit {
 
-  constructor() { }
+  constructor(private data : DataUtenti) { }
 
   ngOnInit() {
   }
+
+  //listaUtenti : DataUtenti[];
+
+  getUtenti() : Utente[] {
+    return this.data.getUtenti();
+  }
+
+  getUtenteById(id : number) : Utente {
+    return this.data.getUtenteById(id);
+  }
+
+
 
 }
---
+++ src/app/user.model.ts
@@ -0,0 +1,3 @@
+import { NULL_EXPR } from "@angular/compiler/src/output/output_ast";
+import { mapToMapExpression } from "@angular/compiler/src/render3/util";
+import { range } from "rxjs";
